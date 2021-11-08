32841ebdb35aa057681d46c843a2cd4666318c39
Cody Zornes <cody@zornes.dev>
2021-11-08 08:50:53 -0800

    add examples from instance and template syntax docs

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log("Hello Vue");
+
+const app = Vue.createApp({
+  data() {
+    return { count: 0,
+              msg: 'hello vue',
+              rawHtml: '<span style="color: red">This should be red.</span>' }
+  },
+  beforeCreate() {
+    console.log('count is null (beforeCreate)');
+  },
+  created() {
+    console.log('count is: ' + ++this.count + '(created)');
+  },
+  beforeMount() {
+    console.log('count is: ' + ++this.count + '(beforeMount)');
+  },
+  mounted() {
+    console.log('count is: ' + ++this.count + '(mounted)');
+  },
+  unmounted() {
+    console.log('count is: ' + ++this.count + '(unmounted)');
+  }
+});
+const vm = app.mount('#app');
+
+//console.log(vm);
+//console.log('count is: ' + ++vm.count);
