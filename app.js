501cd39497a59eb172674f89d3de4a7dd60046ad
Cody Zornes <cody@zornes.dev>
2021-11-09 10:36:03 -0800

    move doSomething to methods option in app component instance

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 console.log("Hello Vue");
 
 const app = Vue.createApp({
+  // Data
   data() {
     return { count: 0,
             msg: 'hello vue',
             rawHtml: '<span style="color: red">This should be red.</span>',
             dynamicId: 'vueId',
             isButtonDisabled: true,
-            linkUrl: null,
-            doSomething: (e) => console.log(`EVENT: click, opening new tab to ${e.target.href} ${e.ctrlKey ? 'in background' : 'in foreground'}.`) 
+            linkUrl: null  
           }
   },
+  // Methods
+  methods: {
+    doSomething() { (e) => console.log(`EVENT: click, opening new tab to ${e.target.href} ${e.ctrlKey ? 'in background' : 'in foreground'}.`) }
+  },
+  // Hooks
   beforeCreate() {
     console.log('count is null (beforeCreate)');
   },
