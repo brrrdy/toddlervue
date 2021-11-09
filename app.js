99595d8e8472e49c29c73194058834b48e940ccb
Cody Zornes <cody@zornes.dev>
2021-11-09 10:53:14 -0800

    add component instance method increment example

--- app.js
+++ app.js
@@ -14,25 +14,28 @@ const app = Vue.createApp({
   // Methods
   // Avoid using arrow functions, which prevents Vue from binding the appropriate "this"
   methods: {
-    doSomething(event) { console.log(`EVENT: click, opening new tab to ${this.linkUrl} ${event.ctrlKey ? ' in background' : 'in foreground'}.`); }
+    doSomething(event) { console.log(`EVENT: click, opening new tab to ${this.linkUrl} ${event.ctrlKey ? ' in background' : 'in foreground'}.`); },
+    increment() {
+      return ++this.count;
+    }
   },
   // Hooks
   beforeCreate() {
     console.log('count is null (beforeCreate)');
   },
   created() {
-    console.log('count is: ' + ++this.count + '(created)');
+    console.log('count is: ' + this.increment() + '(created)');
   },
   beforeMount() {
-    console.log('count is: ' + ++this.count + '(beforeMount)');
+    console.log('count is: ' + this.increment() + '(beforeMount)');
   },
   mounted() {
-    console.log('count is: ' + ++this.count + '(mounted)');
+    console.log('count is: ' + this.increment() + '(mounted)');
   },
   unmounted() {
-    console.log('count is: ' + ++this.count + '(unmounted)');
+    console.log('count is: ' + this.increment() + '(unmounted)');
   }
 });
 const vm = app.mount('#app');
 
-console.log('count is: ' + ++vm.$data.count);
+console.log('count is: ' + vm.increment());
